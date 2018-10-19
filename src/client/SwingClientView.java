1db56e9696a0c2498fe2a503d846e6df777b6154
iregon <0iregon0@gmail.com>
2018-10-19 23:35:58 +0200

    Refactoring

--- src/client/SwingClientView.java
+++ src/client/SwingClientView.java
@@ -28,7 +28,7 @@ public class SwingClientView extends MvpViewAbstract<ClientPresenter> implements
 	// GUI
 	private JFrame frame;
 	private JTextField messageTextField;
-	private JTextField createRoomTextField;
+	private JTextField newRoomTextField;
 	private JTextField changeRoomTextField;
 	
 	JPanel messagesPanel;
@@ -77,14 +77,23 @@ public class SwingClientView extends MvpViewAbstract<ClientPresenter> implements
 		JPanel panel = new JPanel();
 		panel.setLayout(new BorderLayout(0, 0));
 		
-		JPanel chatPanel = new JPanel();
+		JPanel chatPanel = createChatPanel();
 		panel.add(chatPanel);
 		
-		chatPanel.setLayout(new BorderLayout(0, 0));
+		JPanel sendMessagePanel = createSendMessagePanel();
+		panel.add(sendMessagePanel, BorderLayout.SOUTH);
+		
+		return panel;
+	}
+	
+	private JPanel createChatPanel() {
+		JPanel panel = new JPanel();
+		
+		panel.setLayout(new BorderLayout(0, 0));
 		
 		JScrollPane messagesScrollPane = new JScrollPane();
 		messagesScrollPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		chatPanel.add(messagesScrollPane);
+		panel.add(messagesScrollPane);
 		
 		JTextPane messagesTextPane = new JTextPane();
 		messagesTextPane.setBorder(new LineBorder(Color.GRAY));
@@ -95,11 +104,14 @@ public class SwingClientView extends MvpViewAbstract<ClientPresenter> implements
 		messagesLabel.setBorder(new EmptyBorder(0, 5, 5, 5));
 		messagesScrollPane.setColumnHeaderView(messagesLabel);
 		
-		JPanel sendMessagePanel = new JPanel();
-		panel.add(sendMessagePanel, BorderLayout.SOUTH);
+		return panel;
+	}
+	
+	private JPanel createSendMessagePanel() {
+		JPanel panel = new JPanel();
 		
 		messageTextField = new JTextField();
-		sendMessagePanel.add(messageTextField);
+		panel.add(messageTextField);
 		messageTextField.setColumns(30);
 		
 		JButton sendMessageButton = new JButton("Invia");
@@ -107,69 +119,116 @@ public class SwingClientView extends MvpViewAbstract<ClientPresenter> implements
 			public void actionPerformed(ActionEvent arg0) {
 			}
 		});
-		sendMessagePanel.add(sendMessageButton);
+		panel.add(sendMessageButton);
 		
 		return panel;
 	}
 	
 	private JPanel createOptionPanel() {
 		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout(0, 0));
 		
+		JPanel roomsPanel = createRoomsPanel();
+		panel.add(roomsPanel, BorderLayout.CENTER);
+		
+		JTabbedPane roomsTabbedPane = createRoomsTabbedPane();
+		roomsPanel.add(roomsTabbedPane);
+		
+		return panel;
+	}
+	
+	private JTabbedPane createRoomsTabbedPane() {
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		
+		JPanel usersListTabPanel = createUsersListTabPanel();
+		tabbedPane.addTab("Utenti", null, usersListTabPanel, null);
+		
+		JPanel roomsListTabPanel = createRoomsListTabPanel();
+		tabbedPane.addTab("Stanze", null, roomsListTabPanel, null);
+		
+		return tabbedPane;
+	}
+	
+	private JPanel createRoomsPanel() {
+		JPanel panel = new JPanel();
+		
+		panel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		return panel;
+	}	
+	
+	private JPanel createUsersListTabPanel() {
+		JPanel panel = new JPanel();
 		
 		panel.setLayout(new BorderLayout(0, 0));
 		
-		JPanel roomsPanel = new JPanel();
-		roomsPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		panel.add(roomsPanel, BorderLayout.CENTER);
-		roomsPanel.setLayout(new BorderLayout(0, 0));
-		
-		JTabbedPane roomsTabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		roomsPanel.add(roomsTabbedPane);
-		
-		JPanel usersListTabPanel = new JPanel();
-		roomsTabbedPane.addTab("Utenti", null, usersListTabPanel, null);
-		usersListTabPanel.setLayout(new BorderLayout(0, 0));
-		
 		JTextPane usersTextPane = new JTextPane();
 		usersTextPane.setEditable(false);
-		usersListTabPanel.add(usersTextPane, BorderLayout.CENTER);
+		panel.add(usersTextPane, BorderLayout.CENTER);
 		
-		JPanel roomsListTabPanel = new JPanel();
-		roomsTabbedPane.addTab("Stanze", null, roomsListTabPanel, null);
-		roomsListTabPanel.setLayout(new BorderLayout(0, 0));
+		return panel;
+	}
+	
+	private JPanel createRoomsListTabPanel() {
+		JPanel panel = new JPanel();
+
+		panel.setLayout(new BorderLayout(0, 0));
 		
 		JTextPane roomsTextPane = new JTextPane();
 		roomsTextPane.setEditable(false);
-		roomsListTabPanel.add(roomsTextPane, BorderLayout.CENTER);
+		panel.add(roomsTextPane, BorderLayout.CENTER);
 		
-		JPanel roomOptionsPanel = new JPanel();
-		roomsListTabPanel.add(roomOptionsPanel, BorderLayout.SOUTH);
-		roomOptionsPanel.setLayout(new BorderLayout(0, 0));
+		JPanel roomOptionsPanel = createRoomOptionsPanel();
+		panel.add(roomOptionsPanel, BorderLayout.SOUTH);
 		
-		JPanel createRoomPanel = new JPanel();
-		createRoomPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		roomOptionsPanel.add(createRoomPanel, BorderLayout.NORTH);
-		createRoomPanel.setLayout(new BorderLayout(0, 0));
+		return panel;
+	}
+	
+	private JPanel createRoomOptionsPanel() {
+		JPanel panel = new JPanel();
 		
-		createRoomTextField = new JTextField(20);
-		createRoomPanel.add(createRoomTextField, BorderLayout.CENTER);
+		panel.setLayout(new BorderLayout(0, 0));
 		
-		JButton createRoomButton = new JButton("Crea stanza");
-		createRoomButton.setPreferredSize(new Dimension(103, 23));
-		createRoomButton.setMinimumSize(new Dimension(103, 23));
-		createRoomButton.setMaximumSize(new Dimension(103, 23));
-		createRoomPanel.add(createRoomButton, BorderLayout.EAST);
+		JPanel newRoomPanel = createNewRoomPanel();
+		panel.add(newRoomPanel, BorderLayout.NORTH);
 		
-		JPanel changeRoomPanel = new JPanel();
-		changeRoomPanel.setBorder(new EmptyBorder(0, 5, 5, 5));
-		roomOptionsPanel.add(changeRoomPanel);
-		changeRoomPanel.setLayout(new BorderLayout(0, 0));
+		JPanel changeRoomPanel = createChangeRoomPanel();
+		panel.add(changeRoomPanel);
+		
+		return panel;
+	}
+	
+	
+	private JPanel createNewRoomPanel() {
+		JPanel panel = new JPanel();
+		
+		panel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		newRoomTextField = new JTextField(20);
+		panel.add(newRoomTextField, BorderLayout.CENTER);
+		
+		JButton newRoomButton = new JButton("Crea stanza");
+		newRoomButton.setPreferredSize(new Dimension(103, 23));
+		newRoomButton.setMinimumSize(new Dimension(103, 23));
+		newRoomButton.setMaximumSize(new Dimension(103, 23));
+		panel.add(newRoomButton, BorderLayout.EAST);
+		
+		return panel;
+	}
+	
+	private JPanel createChangeRoomPanel() {
+		JPanel panel = new JPanel();
+		
+		panel.setBorder(new EmptyBorder(0, 5, 5, 5));
+		panel.setLayout(new BorderLayout(0, 0));
 		
 		changeRoomTextField = new JTextField(20);
-		changeRoomPanel.add(changeRoomTextField);
+		panel.add(changeRoomTextField);
 		
 		JButton changeRoomButton = new JButton("Cambia stanza");
-		changeRoomPanel.add(changeRoomButton, BorderLayout.EAST);
+		panel.add(changeRoomButton, BorderLayout.EAST);
 		
 		return panel;
 	}
